b9f5c21e44b796cfa4d83b0a2244d70f76a39ae9
Code1deep <az.bahya@gmail.com>
2026-05-04 21:30:29 -0400

    Détermastro.config.mjsochasticité

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import { z } from 'zod';
 
 export default defineConfig({
   site: 'https://code1deep.github.io/dark-debris/',
@@ -8,5 +9,19 @@ export default defineConfig({
   trailingSlash: 'never',
   build: {
     format: 'directory'
+  },
+
+  // Définition des collections avec schemas
+  collections: {
+    blog: {
+      schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        pubDate: z.date(), // propriété attendue comme une date
+        author: z.string(),
+        keywords: z.string(),
+        niveau: z.string()
+      })
+    }
   }
 });
